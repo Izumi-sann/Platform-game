9fda687452e6047ba7418e795b40f59d61b3a954
Izumi-sann <gambalessandro41@gmail.com>
2024-07-25 15:05:33 +0200

    Initial commit

---
+++ prova_2.py
@@ -0,0 +1,4 @@
+if "<Class.Box object at 0x0000020B3FBF0230>" in ["<Class.Box object at 0x0000020B3FBDB680>, <Class.Box object at 0x0000020B3FBDBBC0>, <Class.Box object at 0x0000020B3FBDBD70>, <Class.Box object at 0x0000020B3FBDBFE0>, <Class.Box object at 0x0000020B3FBF0230>"]:
+    print("yes")
+else:
+    print("no")
