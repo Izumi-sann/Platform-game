3eab6b25a12d666c01c9c43914849589ee19ec64
Alessandro <gambalessandro41@gmail.com>
2024-08-07 16:43:59 +0200

    eliminazione file superflui

--- prova_2.py
+++
@@ -1,4 +0,0 @@
-if "<Class.Box object at 0x0000020B3FBF0230>" in ["<Class.Box object at 0x0000020B3FBDB680>, <Class.Box object at 0x0000020B3FBDBBC0>, <Class.Box object at 0x0000020B3FBDBD70>, <Class.Box object at 0x0000020B3FBDBFE0>, <Class.Box object at 0x0000020B3FBF0230>"]:
-    print("yes")
-else:
-    print("no")
